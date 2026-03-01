2a7399cfcb024ddd6b4f29e44219c5db004f3382
flodornuness <mateussoaresnunes00@gmail.com>
2026-03-01 15:11:37 -0300

    criado chat local

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+
+mensagens = []
+
+nome = input("Nome: ")
+
+while True:
+    
+    os.system("cls" if os.name == "nt" else "clear")
+
+    if len(mensagens) > 0:
+        for m in mensagens:
+            print(f"{m['nome']} - {m['texto']}")
+        print("________________")
+
+    texto = input("Mensagem (ou 'fim' para sair): ")
+
+    if texto.lower() == "fim":
+        break
+
+
+    mensagens.append({
+        "nome": nome,
+        "texto": texto
+    })
